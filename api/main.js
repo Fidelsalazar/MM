e8994e8ebfb412630b808497a371d9329d8e859b
Fidel Alejandro Cepero Salazar <fidelsalazar990923@gmail.com>
2024-07-14 14:37:08 -0400

    update: main.ts

--- api/main.js
+++ api/main.js
@@ -4,6 +4,7 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const dotenv = require("dotenv");
 const process = require("node:process");
+const globalexceptionfilter_1 = require("./exceptions/globalexceptionfilter");
 dotenv.config();
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
@@ -14,6 +15,7 @@ async function bootstrap() {
         credentials: true,
         allowedHeaders: '*',
     });
+    app.useGlobalFilters(new globalexceptionfilter_1.GlobalExceptionFilter());
     await app.listen(port);
     console.log(`Application is running on: ${await app.getUrl()}`);
 }
